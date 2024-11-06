74584baa4bd3c713cf6310df8eb755d87319e2bf
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-06 15:04:14 +1300

    added route to homepage in App

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,7 +1,7 @@
 import styles from "./homepage.module.css";
 import Navbar from "./components/Navbar.jsx";
 
-export default function homepage() {
+export default function Homepage() {
   return (
     <div className={styles.main}>
       <Navbar />
