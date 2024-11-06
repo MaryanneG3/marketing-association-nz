497853a73463294678e7ec0ebe5a8afb29778703
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-06 17:21:49 +1300

    added styling to homepage

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,10 +1,12 @@
 import styles from "./homepage.module.css";
 import Navbar from "./components/Navbar.jsx";
+import MainContent from "./components/MainContent.jsx";
 
 export default function Homepage() {
   return (
     <div className={styles.main}>
       <Navbar />
+      <MainContent />
     </div>
   );
 }
