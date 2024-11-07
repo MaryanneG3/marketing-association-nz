b052fbcd9db90e196cb3feff5c27b21700a5b84f
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-08 04:05:29 +1300

    installed Montserrat font

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,6 +1,7 @@
 import styles from "./homepage.module.css";
 import Navbar from "./components/Navbar.jsx";
 import MainContent from "./components/MainContent.jsx";
+import "@fontsource/montserrat/400.css";
 
 export default function Homepage() {
   return (
