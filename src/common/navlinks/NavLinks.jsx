81a6a37b49828bb983459d156f8fb5505af18988
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-08 16:33:08 +1300

    updated nav links styling

--- src/common/navlinks/NavLinks.jsx
+++ src/common/navlinks/NavLinks.jsx
@@ -12,19 +12,21 @@ function NavLinks({ customClass, iconsOnly }) {
   return (
     <div className={customClass}>
       <NavLink className={styles.navlink} to="/">
-        {iconsOnly && <FontAwesomeIcon icon={faHome} />}
+        {iconsOnly && <FontAwesomeIcon icon={faHome} className={styles.icon} />}
         {!iconsOnly && <span className={styles.navText}>Home</span>}
       </NavLink>
       <NavLink className={styles.navlink} to="/contact">
-        {iconsOnly && <FontAwesomeIcon icon={faInfoCircle} />}
+        {iconsOnly && (
+          <FontAwesomeIcon icon={faInfoCircle} className={styles.icon} />
+        )}
         {!iconsOnly && <span className={styles.navText}>Contact</span>}
       </NavLink>
       <NavLink className={styles.navlink} to="/about">
-        {iconsOnly && <FontAwesomeIcon icon={faCogs} />}
+        {iconsOnly && <FontAwesomeIcon icon={faCogs} className={styles.icon} />}
         {!iconsOnly && <span className={styles.navText}>About</span>}
       </NavLink>
       <NavLink className={styles.navlink} id={styles.loginNavlink} to="/login">
-        {iconsOnly && <FontAwesomeIcon icon={faUser} />}
+        {iconsOnly && <FontAwesomeIcon icon={faUser} className={styles.icon} />}
         {!iconsOnly && <span className={styles.navText}>Login</span>}
       </NavLink>
     </div>
