bcc42bdf0b7f1ad51195876378942384d4684cd3
Maryanne G <maryanneg@missionreadyhq.com>
2024-11-08 15:56:46 +1300

    applied conditional rendering to default navlinks so that when iconsOnly prop is true then only icons, no text will display, else if flase then only text will display

--- src/common/navlinks/NavLinks.jsx
+++ src/common/navlinks/NavLinks.jsx
@@ -12,19 +12,19 @@ function NavLinks({ customClass, iconsOnly }) {
   return (
     <div className={customClass}>
       <NavLink className={styles.navlink} to="/">
-        <FontAwesomeIcon icon={faHome} />
+        {iconsOnly && <FontAwesomeIcon icon={faHome} />}
         {!iconsOnly && <span className={styles.navText}>Home</span>}
       </NavLink>
       <NavLink className={styles.navlink} to="/contact">
-        <FontAwesomeIcon icon={faInfoCircle} />
+        {iconsOnly && <FontAwesomeIcon icon={faInfoCircle} />}
         {!iconsOnly && <span className={styles.navText}>Contact</span>}
       </NavLink>
       <NavLink className={styles.navlink} to="/about">
-        <FontAwesomeIcon icon={faCogs} />
+        {iconsOnly && <FontAwesomeIcon icon={faCogs} />}
         {!iconsOnly && <span className={styles.navText}>About</span>}
       </NavLink>
       <NavLink className={styles.navlink} id={styles.loginNavlink} to="/login">
-        <FontAwesomeIcon icon={faUser} />
+        {iconsOnly && <FontAwesomeIcon icon={faUser} />}
         {!iconsOnly && <span className={styles.navText}>Login</span>}
       </NavLink>
     </div>
